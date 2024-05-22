95d16845a7999c773a57fa1e8618b9ad36688788
kenzo-b <contact@kbriche.fr>
2024-05-23 01:46:16 +0200

    service entity finished

---
+++ GestMegots/Entitees/Service.cs
@@ -0,0 +1,47 @@
+﻿namespace GestMegots.Entitees;
+
+public class Service
+{
+    private int id;
+    private string nom;
+    
+    public int Id { get => id ; set => id = value;}
+    public string Nom{get => nom ; set => nom = value;}
+
+    public class Builder
+    {
+        private int id;
+        private string nom;
+    
+        public int Id { get => id ; set => id = value;}
+        public string Nom{get => nom ; set => nom = value;}
+
+        public Builder WithId(int id)
+        {
+            Id = id;
+            return this;
+        }
+
+        public Builder WithNom(string nom)
+        {
+            Nom = nom;
+            return this;
+        }
+
+        public Service build()
+        {
+            return new Service(this);
+        }
+    }
+
+    private Service(Builder builder)
+    {
+        Id = builder.Id;
+        Nom = builder.Nom;
+    }
+
+    public override string ToString()
+    {
+        return Nom;
+    }
+}
