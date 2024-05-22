eceeef6a87d18af82d7a3bc32fc5ff6141c5e66d
kenzo-b <contact@kbriche.fr>
2024-05-23 01:48:09 +0200

    some change

--- GestMegots/Modeles/MaterielModele.cs
+++ GestMegots/Modeles/MaterielModele.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GestMegots.Entitees;
+﻿using GestMegots.Entitees;
 using MySql.Data.MySqlClient;
 
 namespace GestMegots.Modeles
