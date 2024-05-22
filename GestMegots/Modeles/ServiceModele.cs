c3ec2c8a88f90ef7d33d2b00948283e4940023a9
kenzo-b <contact@kbriche.fr>
2024-05-23 01:46:45 +0200

    both model new model finished

---
+++ GestMegots/Modeles/ServiceModele.cs
@@ -0,0 +1,43 @@
+﻿using GestMegots.Entitees;
+using MySql.Data.MySqlClient;
+
+namespace GestMegots.Modeles;
+
+public class ServiceModele
+{
+    private static Service ReaderToService(MySqlDataReader lecteur)
+    {
+        return new Service.Builder()
+            .WithId(int.Parse(lecteur[0].ToString()))
+            .WithNom(lecteur[1].ToString())
+            .build();
+    }
+    
+    public static List<Service> ToutLesServices()
+    {
+        List<Service> lesServ = new List<Service>();
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "SELECT * FROM service";
+        MySqlDataReader Lecteur = cmd.ExecuteReader();
+        while(Lecteur.Read())
+        {
+            lesServ.Add(ReaderToService(Lecteur));
+        }
+        connex.Close();
+        return lesServ;
+    }
+    
+    public static Service GetServiceById(int id)
+    {
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "SELECT * FROM service WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        MySqlDataReader lecteur = cmd.ExecuteReader();
+        lecteur.Read();
+        Service unServ = ReaderToService(lecteur);
+        connex.Close();
+        return unServ;
+    }
+}
---
+++ GestMegots/Modeles/UserModel.cs
@@ -0,0 +1,84 @@
+﻿using GestMegots.Entitees;
+using MySql.Data.MySqlClient;
+
+namespace GestMegots.Modeles;
+
+public class UserModel
+{
+    public static User ReaderToUser(MySqlDataReader lecteur)
+    {
+        return new User.Builder()
+            .WithId(int.Parse(lecteur[0].ToString()))
+            .WithPseudo(lecteur[1].ToString())
+            .WithPasswd(lecteur[2].ToString())
+            .WithService(ServiceModele.GetServiceById(int.Parse(lecteur[3].ToString())))
+            .WithHabLevel(int.Parse(lecteur[4].ToString()))
+            .build();
+    }
+    
+    public static List<User> ToutLesUsers()
+    {
+        List<User> lesServ = new List<User>();
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "SELECT * FROM user";
+        MySqlDataReader lecteur = cmd.ExecuteReader();
+        while(lecteur.Read())
+        {
+            lesServ.Add(ReaderToUser(lecteur));
+        }
+        connex.Close();
+        return lesServ;
+    }
+    
+    public static User GetUserById(int id)
+    {
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "SELECT * FROM user WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        MySqlDataReader lecteur = cmd.ExecuteReader();
+        lecteur.Read();
+        User unServ = ReaderToUser(lecteur);
+        connex.Close();
+        return unServ;
+    }
+    
+    public static void AjouterUser(User user)
+    {
+
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "INSERT INTO user(pseudo, passwd, fk_service, hab_level) VALUE (@pseudo, @passwd, @fk_service, @hab_level)";
+        cmd.Parameters.AddWithValue("@pseudo", user.Pseudo);
+        cmd.Parameters.AddWithValue("@passwd", user.Passwd);
+        cmd.Parameters.AddWithValue("@fk_service", user.Service.Id);
+        cmd.Parameters.AddWithValue("@hab_level", user.Habilitation);
+        cmd.ExecuteNonQuery();
+        connex.Close();
+    }
+    
+    public static void SupprimerUser(User user)
+    {
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "DELETE FROM user WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", user.Id);
+        cmd.ExecuteNonQuery();
+        connex.Close();
+    }
+    
+    public static void ChangerUser(User user)
+    {
+        MySqlConnection connex = Connection.Ouvrir();
+        MySqlCommand cmd = connex.CreateCommand();
+        cmd.CommandText = "UPDATE User SET pseudo = @pseudo, passwd = @passwd, fk_service = @fk_service, hab_level = @hab_level WHERE id = @id";
+        cmd.Parameters.AddWithValue("@pseudo", user.Pseudo);
+        cmd.Parameters.AddWithValue("@passwd", user.Passwd);
+        cmd.Parameters.AddWithValue("@fk_service", user.Service.Id);
+        cmd.Parameters.AddWithValue("@hab_level", user.Habilitation);
+        cmd.Parameters.AddWithValue("@id", user.Id);
+        cmd.ExecuteNonQuery();
+        connex.Close(); 
+    }
+}
