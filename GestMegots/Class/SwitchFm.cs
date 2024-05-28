e50a7d582768b065816813c441dabf6201717b76
kenzo-b <contact@kbriche.fr>
2024-05-28 23:10:56 +0200

    made function that hundle all current form and permit to switch to another form by passing an enum

---
+++ GestMegots/Class/SwitchFm.cs
@@ -0,0 +1,24 @@
+﻿using GestMegots.Formulaires;
+
+namespace GestMegots.Class;
+
+public class SwitchFm
+{
+    public enum Forms
+    {
+        FmHotspot,
+        FmMateriel,
+        FmCollecte,
+        FmUser
+    }
+    
+    public static void To(Forms form)
+    {
+        Form currentForm = Form.ActiveForm;
+        if (currentForm != null)
+        {
+            ((Form)Activator.CreateInstance(Type.GetType($"GestMegots.Formulaires.{form.ToString()}"))).Show();
+            currentForm.Hide();
+        }
+    }
+}
