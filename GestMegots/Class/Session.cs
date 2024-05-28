5017c3efb3af23869e1b1836ca9cb634ff5aa24a
kenzo-b <contact@kbriche.fr>
2024-05-28 23:12:42 +0200

    static class that permit to get habilitation level of the current loged user between different form

---
+++ GestMegots/Class/Session.cs
@@ -0,0 +1,9 @@
+﻿namespace GestMegots.Class;
+
+public static class Session
+{
+    private static int _sessionHab;
+    private static bool _loggedIn;
+    public static int SessionHab { get => _sessionHab; set => _sessionHab = value; }
+    public static bool LoggedIn { get => _loggedIn; set => _loggedIn = value; }
+}
