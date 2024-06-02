5e17722aeb3804310709763e35b8c9113812f118
kenzo-b <contact@kbriche.fr>
2024-06-02 14:26:17 +0200

    add the pseudo to the session and only do the getter instead of both setter and getter. also made a func to set a session and unset it

--- GestMegots/Class/Session.cs
+++ GestMegots/Class/Session.cs
@@ -2,8 +2,27 @@
 
 public static class Session
 {
+    public static int SessionHab { get => _sessionHab; }
+
+    public static bool LoggedIn { get => _loggedIn; }
+
+    public static string? Pseudo { get => _pseudo; }
+
     private static int _sessionHab;
     private static bool _loggedIn;
-    public static int SessionHab { get => _sessionHab; set => _sessionHab = value; }
-    public static bool LoggedIn { get => _loggedIn; set => _loggedIn = value; }
+    private static string? _pseudo;
+
+    public static void SetSession(int sessionHab, bool loggedIn, string pseudo )
+    {
+        _sessionHab = sessionHab;
+        _loggedIn = loggedIn;
+        _pseudo = pseudo;
+    }
+
+    public static void UnsetSession()
+    {
+        _sessionHab = 0;
+        _loggedIn = false;
+        _pseudo = string.Empty;
+    }
 }
