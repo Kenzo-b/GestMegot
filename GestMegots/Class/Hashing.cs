29ee1f54a60862c448da647c02633b036fa592d9
kenzo-b <contact@kbriche.fr>
2024-06-02 14:21:47 +0200

    code optimization

--- GestMegots/Class/Hashing.cs
+++ GestMegots/Class/Hashing.cs
@@ -1,19 +1,18 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace GestMegots;
+namespace GestMegots.Class;
 
-public class Hashing
+public static class Hashing
 {
     public static string ToSha256(string s)
     {
         using var sha256 = SHA256.Create();
         byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
-
         var sb = new StringBuilder();
-        for (int i = 0; i < bytes.Length; i++)
+        foreach (var t in bytes)
         {
-            sb.Append(bytes[i].ToString("x2"));
+            sb.Append(t.ToString("x2"));
         }
         return sb.ToString();
     }
