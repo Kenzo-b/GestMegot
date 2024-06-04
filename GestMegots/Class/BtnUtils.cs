e2f2078c99e998d0b554b57c5b703173a0a3b420
kenzo-b <contact@kbriche.fr>
2024-06-04 23:35:28 +0200

    add BtnUtils class with 2 function, one to set the visibility and the other to show and get the result of a messageBox used with button

---
+++ GestMegots/Class/BtnUtils.cs
@@ -0,0 +1,34 @@
+﻿
+namespace GestMegots.Class
+{
+    public static class BtnUtils
+    {
+        public static void SetButtonVisibility(Form currentForm)
+        {
+            foreach (var requirement in BtnAbilityRequirement)
+            {
+                Control[] foundControls = currentForm.Controls.Find(requirement.Key, true);
+                if (foundControls[0] is Button button)
+                {
+                    button.Visible = requirement.Value <= Session.SessionHab;
+                }
+            }
+        }
+
+        private static readonly Dictionary<string, int> BtnAbilityRequirement = new()
+        {
+            { "bt_add", 1 },
+            { "bt_dell", 2 },
+            { "bt_update", 2 },
+            { "bt_collect", 1 },
+            { "bt_hotspot", 2 },
+            { "bt_materiel", 2 },
+            { "bt_user", 3 },
+        };
+
+        public static bool VerifyDecision()
+        {
+            return MessageBox.Show(@"comfirmez vous cette action?", null, MessageBoxButtons.YesNo) == DialogResult.Yes;
+        }
+    }
+}
