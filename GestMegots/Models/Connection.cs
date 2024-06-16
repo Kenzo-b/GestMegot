5cb0f7ec46a89e0afaff57482be061e11c4a7570
kenzo-b <contact@kbriche.fr>
2024-06-16 23:09:56 +0200

    comment added

--- GestMegots/Models/Connection.cs
+++ GestMegots/Models/Connection.cs
@@ -4,6 +4,8 @@ namespace GestMegots.Models
 {
     public static class Connection
     {
+        //change and add your DB information
+        
         private const string Server = "localhost";
         private const string Port = "3306";
         private const string Database = "gest_megot";
